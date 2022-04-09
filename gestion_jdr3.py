169e17ff1628bbca6b40fa44646df757d5d41063
French-isotope <french-isotope@gmail.com>
2022-04-09 09:32:50 +0200

    refactor sounds dir handling for avoiding repetitions

--- gestion_jdr3.py
+++ gestion_jdr3.py
@@ -12,6 +12,7 @@ SCREEN_WIDTH = 600
 TYPO_DEFAULT_SIZE = 20
 
 BASE_URL = "C:/projects/soundpyd"
+SOUNDS_DIR = f"{BASE_URL}/sounds"
 
 pygame.init()
 screen = pygame.display.set_mode((SCREEN_HEIGHT, SCREEN_WIDTH))
@@ -23,35 +24,35 @@ buttons = [
         "coords": (400, 300),
         "color": IMAGE_NORMAL_COLOR,
         "size": TYPO_DEFAULT_SIZE,
-        "url": f"{BASE_URL}/sounds/MSE.mp3",
+        "url": f"{SOUNDS_DIR}/MSE.mp3",
     },
     {
         "name": "war",
         "coords": (300, 300),
         "color": IMAGE_NORMAL_COLOR,
         "size": TYPO_DEFAULT_SIZE,
-        "url": f"{BASE_URL}/sounds/FSW.mp3",
+        "url": f"{SOUNDS_DIR}/FSW.mp3",
     },
     {
         "name": "cavern",
         "coords": (200, 300),
         "color": IMAGE_NORMAL_COLOR,
         "size": TYPO_DEFAULT_SIZE,
-        "url": f"{BASE_URL}/sounds/SC.mp3",
+        "url": f"{SOUNDS_DIR}/SC.mp3",
     },
     {
         "name": "house_with_fire",
         "coords": (000, 300),
         "color": IMAGE_NORMAL_COLOR,
         "size": TYPO_DEFAULT_SIZE,
-        "url": f"{BASE_URL}/sounds/house_with_fire.mp3",
+        "url": f"{SOUNDS_DIR}/house_with_fire.mp3",
     },
     {
         "name": "joy_festival",
         "coords": (400, 0),
         "color": IMAGE_NORMAL_COLOR,
         "size": TYPO_DEFAULT_SIZE,
-        "url": f"{BASE_URL}/sounds/joy_festival.mp3",
+        "url": f"{SOUNDS_DIR}/joy_festival.mp3",
     },
 ]
 

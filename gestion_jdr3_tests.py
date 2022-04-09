4565a14b3b94bf9116f145f18ae870e56432f5b5
French-isotope <french-isotope@gmail.com>
2022-04-09 09:48:03 +0200

    refactor tests file for image_buttons, whiping all useless functions

--- gestion_jdr3_tests.py
+++ gestion_jdr3_tests.py
@@ -12,18 +12,22 @@ SCREEN_WIDTH = 600
 TYPO_DEFAULT_SIZE = 20
 
 BASE_URL = "C:/projects/soundpyd"
+SOUNDS_DIR = f"{BASE_URL}/sounds"
+IMG_DIR = f"{BASE_URL}/img_resized"
+
 
 pygame.init()
 screen = pygame.display.set_mode((SCREEN_HEIGHT, SCREEN_WIDTH))
 pygame.display.set_caption("Soundpyd")
 
-buttons = [
+the_buttons = [
     {
         "name": "cavern",
         "coords": (200, 300),
         "color": IMAGE_NORMAL_COLOR,
         "size": TYPO_DEFAULT_SIZE,
-        "url": f"{BASE_URL}/sounds/SC.mp3",
+        "url": f"{SOUNDS_DIR}/SC.mp3",
+        "img": f"{IMG_DIR}/cavern_rs.png",
     },
 ]
 
@@ -45,56 +49,15 @@ def toggle_sound(b, is_playing, soundname, sound, fade_time=4000):
         return is_playing
 
 
-def text_button(screen, position, text, size, colors="white on blue"):
-    fg, bg = colors.split(" on ")
-    font = pygame.font.SysFont("Calibri", size)
-    text_render = font.render(text, 1, fg)
-    x, y, w, h = text_render.get_rect()
-    x, y = position
-    pygame.draw.line(screen, (150, 150, 150), (x, y), (x + w, y), 5)
-    pygame.draw.line(screen, (150, 150, 150), (x, y - 2), (x, y + h), 5)
-    pygame.draw.line(screen, (50, 50, 50), (x, y + h), (x + w, y + h), 5)
-    pygame.draw.line(screen, (50, 50, 50), (x + w, y + h), [x + w, y], 5)
-    pygame.draw.rect(screen, bg, (x, y, w, h))
-    #    print(screen.blit(text_render, (x, y)))
-    return screen.blit(text_render, (x, y))
-
-
-def action_on_click(b, button_var, is_playing):
-    if b.collidepoint(pygame.mouse.get_pos()):
-        return change_color_down(button_var, is_playing)
-    else:
-        return b
-
-
-def change_color_down(button_var, is_playing):
-    if is_playing:
-        return text_button(screen, button_var["coords"], button_var["name"], button_var["size"], IMAGE_DOWN_COLOR)
-    else:
-        return text_button(screen, button_var["coords"], button_var["name"], button_var["size"], button_var["color"])
-
-
-def change_color_over(b, button_var, is_playing):
-    if b.collidepoint(pygame.mouse.get_pos()) and not is_playing:
-        return text_button(screen, button_var["coords"], button_var["name"], button_var["size"], IMAGE_HOVER_COLOR)
-    elif not b.collidepoint(pygame.mouse.get_pos()) and not is_playing:
-        return text_button(screen, button_var["coords"], button_var["name"], button_var["size"], button_var["color"])
-    else:
-        return b
-
-
-
 def menu(buttons):
     """ This is the menu that waits you to click the buttons to start playing sounds"""
 
-    buttons_states = dict()
     sounds = dict()
     is_playing = dict()
 
     for b in buttons:
         b_name = b["name"]
         is_playing[b_name] = False
-        buttons_states[b_name] = text_button(screen, b["coords"], b_name, b["size"], b["color"])
         sounds[b_name] = create_sound(b["url"])
         print(is_playing[b_name])
 
@@ -103,15 +66,11 @@ def menu(buttons):
 
             if event.type == pygame.MOUSEBUTTONDOWN:
                 for b in buttons:
-                    b_name = b["name"]
-                    b_state = buttons_states[b_name]
-                    is_playing[b_name] = toggle_sound(b_state, is_playing[b_name], b_name, sounds[b_name], 4000)
-                    buttons_states[b_name] = action_on_click(b_state, b, is_playing[b_name])
+                    pass
 
             elif event.type == pygame.MOUSEMOTION:
                 for b in buttons:
-                    b_name = b["name"]
-                    buttons_states[b_name] = change_color_over(buttons_states[b_name], b, is_playing[b_name])
+                    pass
 
             elif event.type == pygame.QUIT:
                 pygame.quit()
@@ -120,4 +79,4 @@ def menu(buttons):
 
 
 if __name__ == "__main__":
-    menu(buttons)
+    menu(the_buttons)
